7d397af0c90ed865ea287b7b0c6c372a760e91c7
sylwia <sylwiavv@gmail.com>
2024-06-07 12:58:35 +0200

    Project init

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  template:
+      `<h1 class="text-3xl font-bold underline">
+    Welcome to {{title}}!</h1>
+
+    <router-outlet />
+  `,
+  styles: [],
+})
+export class AppComponent {
+  title = 'cat-in-boots';
+}
